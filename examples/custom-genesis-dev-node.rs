29cf96b064fbf4bd068be3d7d143a25df02724e6
Jrachman <rachmanjulian8@gmail.com>
2024-04-23 14:58:45 -0700

    created ability to programmatically spin up an alphanet dev node with hardfork error workaround

--- examples/custom-genesis-dev-node.rs
+++ examples/custom-genesis-dev-node.rs
@@ -1,17 +1,18 @@
-//! This example shows how to run a custom dev node programmatically and submit a transaction
+//! This example shows how to run a custom dev node programmatically
 //! through rpc.
 
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-use futures_util::StreamExt;
 use reth::{
-    args::DevArgs, builder::{NodeBuilder, NodeHandle}, providers::CanonStateSubscriptions, rpc::eth::EthTransactions, tasks::TaskManager
+    args::DevArgs, builder::NodeBuilder, tasks::TaskManager
 };
 use reth_node_core::{args::RpcServerArgs, node_config::NodeConfig};
-use reth_node_ethereum::EthereumNode;
-use reth_primitives::{b256, hex, ChainSpec, Genesis};
+use reth_node_optimism::{args::RollupArgs, OptimismNode};
+use reth_primitives::{ChainSpec, ForkCondition, ForkTimestamps, Genesis, GenesisAccount, Hardfork};
 use reth_tracing::{RethTracer, Tracer};
+use revm_primitives::{address, U256};
 use std::{sync::Arc, time::Duration};
+use alphanet_node::node::AlphaNetNode;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -31,7 +32,8 @@ async fn main() -> eyre::Result<()> {
 
     let handle = NodeBuilder::new(node_config)
         .testing_node(tasks.executor())
-        .node(EthereumNode::default())
+        .with_types(AlphaNetNode::default())
+        .with_components(OptimismNode::components(RollupArgs::default()))
         .launch()
         .await
         .unwrap();
@@ -42,42 +44,32 @@ async fn main() -> eyre::Result<()> {
 }
 
 fn custom_chain() -> Arc<ChainSpec> {
-    let custom_genesis = r#"
-{
-    "nonce": "0x42",
-    "timestamp": "0x0",
-    "extraData": "0x5343",
-    "gasLimit": "0x1388",
-    "difficulty": "0x400000000",
-    "mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "coinbase": "0x0000000000000000000000000000000000000000",
-    "alloc": {
-        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266": {
-            "balance": "0x4a47e3c12448f4ad000000"
-        }
-    },
-    "number": "0x0",
-    "gasUsed": "0x0",
-    "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "config": {
-        "ethash": {},
-        "chainId": 2600,
-        "homesteadBlock": 0,
-        "eip150Block": 0,
-        "eip155Block": 0,
-        "eip158Block": 0,
-        "byzantiumBlock": 0,
-        "constantinopleBlock": 0,
-        "petersburgBlock": 0,
-        "istanbulBlock": 0,
-        "berlinBlock": 0,
-        "londonBlock": 0,
-        "terminalTotalDifficulty": 0,
-        "terminalTotalDifficultyPassed": true,
-        "shanghaiTime": 0
-    }
-}
-"#;
-    let genesis: Genesis = serde_json::from_str(custom_genesis).unwrap();
-    Arc::new(genesis.into())
+    let custom_genesis = include_str!("../etc/alphanet-genesis.json");
+    let mut genesis: Genesis = serde_json::from_str(custom_genesis).unwrap();
+    genesis.alloc.insert(
+        address!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+        GenesisAccount {
+            balance: U256::MAX,
+            nonce: None,
+            code: None,
+            storage: None,
+            private_key: None,
+        },
+    );
+    let mut chainspec: ChainSpec = ChainSpec {
+        fork_timestamps: ForkTimestamps::default()
+            .shanghai(1699981200)
+            .canyon(1699981200)
+            .cancun(1708534800)
+            .ecotone(1708534800),
+        ..genesis.into()
+    };
+    chainspec.hardforks.insert(Hardfork::Bedrock, ForkCondition::Block(0));
+    chainspec.hardforks.insert(Hardfork::Regolith, ForkCondition::Timestamp(0));
+    chainspec.hardforks.insert(Hardfork::Shanghai, ForkCondition::Timestamp(1699981200));
+    chainspec.hardforks.insert(Hardfork::Canyon, ForkCondition::Timestamp(1699981200));
+    chainspec.hardforks.insert(Hardfork::Cancun, ForkCondition::Timestamp(1708534800));
+    chainspec.hardforks.insert(Hardfork::Ecotone, ForkCondition::Timestamp(1708534800));
+
+    Arc::new(chainspec)
 }
