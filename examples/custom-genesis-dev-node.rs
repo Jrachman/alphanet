ac5f3b0f5628715891e1bafa554d0f4c6cd2d44f
Jrachman <rachmanjulian8@gmail.com>
2024-05-08 20:44:51 -0700

    updated script to match integration test setup

--- examples/custom-genesis-dev-node.rs
+++ examples/custom-genesis-dev-node.rs
@@ -8,17 +8,18 @@ use reth::{
 };
 use reth_node_core::{args::RpcServerArgs, node_config::NodeConfig};
 use reth_node_optimism::args::RollupArgs;
-use reth_primitives::{ChainSpec, ForkCondition, Genesis, GenesisAccount, Hardfork};
+use reth_primitives::DEV;
 use reth_tracing::{RethTracer, Tracer};
-use revm_primitives::{address, U256};
-use std::{net::{IpAddr, Ipv4Addr}, sync::Arc, time::Duration};
+use std::{net::{IpAddr, Ipv4Addr}, time::Duration};
 use alphanet_node::node::AlphaNetNode;
+use once_cell::sync::Lazy;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let _guard = RethTracer::new().init()?;
-    
     let tasks = TaskManager::current();
+
+    let chain_spec = Lazy::force(&DEV).clone();
 
     // create node config
     let node_config = NodeConfig::test()
@@ -40,11 +41,11 @@ async fn main() -> eyre::Result<()> {
             ].to_vec())),
             ..RpcServerArgs::default().with_http().with_ws()
         })
-        .with_chain(custom_chain());
+        .with_chain(chain_spec);
 
     let handle = NodeBuilder::new(node_config)
         .testing_node(tasks.executor())
-        .node(AlphaNetNode::new(RollupArgs::default()))
+        .node(AlphaNetNode::default())
         .launch()
         .await
         .unwrap();
@@ -52,30 +53,4 @@ async fn main() -> eyre::Result<()> {
     println!("Node started");
 
     handle.node_exit_future.await
-}
-
-fn custom_chain() -> Arc<ChainSpec> {
-    let custom_genesis = include_str!("../etc/alphanet-genesis.json");
-    let mut genesis: Genesis = serde_json::from_str(custom_genesis).unwrap();
-    genesis.alloc.insert(
-        address!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
-        GenesisAccount {
-            balance: U256::MAX,
-            nonce: None,
-            code: None,
-            storage: None,
-            private_key: None,
-        },
-    );
-    let mut chainspec: ChainSpec = ChainSpec {
-        ..genesis.into()
-    };
-    chainspec.hardforks.insert(Hardfork::Bedrock, ForkCondition::Block(0));
-    chainspec.hardforks.insert(Hardfork::Regolith, ForkCondition::Timestamp(0));
-    chainspec.hardforks.insert(Hardfork::Shanghai, ForkCondition::Timestamp(1699981200));
-    chainspec.hardforks.insert(Hardfork::Canyon, ForkCondition::Timestamp(1699981200));
-    chainspec.hardforks.insert(Hardfork::Cancun, ForkCondition::Timestamp(1708534800));
-    chainspec.hardforks.insert(Hardfork::Ecotone, ForkCondition::Timestamp(1708534800));
-
-    Arc::new(chainspec)
 }
